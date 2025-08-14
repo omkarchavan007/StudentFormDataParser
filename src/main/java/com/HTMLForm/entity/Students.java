1b5a774f31e32c647fa1b78bbc98571ca0e413cf
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-14 11:06:30 +0530

    Configured Students Entity

--- src/main/java/com/HTMLForm/entity/Students.java
+++ src/main/java/com/HTMLForm/entity/Students.java
@@ -4,14 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 
-@Getter
-@Setter
+
 @ToString
 
 public class Students {
@@ -20,15 +17,85 @@ public class Students {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private String FirstName;
-    private  String LastName;
-    private String Email;
-    private String Gender;
-    private String City;
-    private String Course;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private String city;
+    private String course;
 
-    private int PassoutYear;
-    private String Branch;
+    private int passoutYear;
+    private String branch;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getPassoutYear() {
+        return passoutYear;
+    }
+
+    public void setPassoutYear(int passoutYear) {
+        this.passoutYear = passoutYear;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
 }
