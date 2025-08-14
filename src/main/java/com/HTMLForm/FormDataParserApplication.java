4f0fc85a6ce5d4cc3d174328bf27b3f2aa6eb561
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-14 09:00:32 +0530

    created Student data parser

---
+++ src/main/java/com/HTMLForm/FormDataParserApplication.java
@@ -0,0 +1,13 @@
+package com.HTMLForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormDataParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormDataParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HTMLForm/FormDataParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HTMLForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormDataParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
